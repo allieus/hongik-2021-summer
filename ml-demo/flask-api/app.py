52c3d62d9a80271c364c15d4c401eef859f19dfb
Chinseok Lee <me@askcompany.kr>
2021-08-03 18:16:36 +0900

    mldemo form 페이지 구현

--- ml-demo/flask-api/app.py
+++ ml-demo/flask-api/app.py
@@ -5,7 +5,30 @@ import ml
 app = Flask(__name__)
 
 # 템플릿 자동 리로딩
-# app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+
+# 브라우저로 http://localhost:5000 주소 접속하시면, hello world 메세지
+
+
+@app.route('/', methods=["GET"])
+def index():
+    return render_template("index.html")
+
+    # return """
+    # <html>
+    #     <head>
+    #         <style>
+    #             body {
+    #                 background-color: red;
+    #             }
+    #         </style>
+    #     </head>
+    #     <body>
+    #         Hello  World
+    #     </body>
+    # </html>
+    # """
 
 
 @app.route("/predict", methods=["POST"])
@@ -14,4 +37,5 @@ def predict():
     for name, f in request.files.items():
         expected_number = ml.predict(f)
         expected[name] = expected_number
+
     return jsonify(expected)
