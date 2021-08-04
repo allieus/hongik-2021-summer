38589f33798d870be9b7a60c74d047b34b96b700
Chinseok Lee <me@askcompany.kr>
2021-08-04 16:24:10 +0900

    [travel] index 뷰 구현

--- django_proj1/travel/urls.py
+++ django_proj1/travel/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 from travel import views
 
 urlpatterns = [
+    path("", views.index, name="index"),
 ]
--- django_proj1/travel/views.py
+++ django_proj1/travel/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from travel.models import Post
+
+
+def index(request):
+    qs = Post.objects.all()
+    return render(request, "travel/post_list.html", {
+        "post_list": qs,
+    })
