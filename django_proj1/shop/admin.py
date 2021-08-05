f7832f8ebd880d06fae4a974830252f99d078141
Chinseok Lee <me@askcompany.kr>
2021-08-05 11:54:19 +0900

    [shop] Item 모델 생성 및 마이그레이션, admin에 등록

--- django_proj1/shop/admin.py
+++ django_proj1/shop/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from shop.models import Item
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ['name', 'price', 'is_public', 'created_at']
+    list_filter = ['is_public', 'created_at']
