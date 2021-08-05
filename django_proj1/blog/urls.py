f88ccf83d4a2faec8dcce318c550a70b1820bda6
Chinseok Lee <me@askcompany.kr>
2021-08-05 10:36:02 +0900

    blog 샘플 구현

--- django_proj1/blog/urls.py
+++ django_proj1/blog/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
+    path("", views.index, name="index"),
+    path("<int:pk>/", views.article_detail, name="article_detail"),
 ]
--- django_proj1/blog/views.py
+++ django_proj1/blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from blog.models import Article
+
+
+def index(request):
+    qs = Article.objects.all()
+    return render(request, "blog/article_list.html", {
+        "article_list": qs,
+    })
+
+
+def article_detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    return render(request, "blog/article_detail.html", {
+        "article": article,
+    })
