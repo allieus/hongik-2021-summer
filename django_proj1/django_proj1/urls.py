3e80a16a62d96bf4e9960ee778cab430fc53eb6f
Chinseok Lee <me@askcompany.kr>
2021-08-05 12:07:00 +0900

    [shop] MEDIA 세팅 및 Item.photo 이미지 필드 추가

--- django_proj1/django_proj1/urls.py
+++ django_proj1/django_proj1/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -7,3 +9,5 @@ urlpatterns = [
     path('shop/', include('shop.urls')),
     path('travel/', include('travel.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
