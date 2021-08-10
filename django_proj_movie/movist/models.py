a9f2d985cbbe77930202fb1b43dca9bfa84f599c
Chinseok Lee <me@askcompany.kr>
2021-08-10 11:15:47 +0900

    [movist] Actor/Movie/Video 모델 생성 및 admin 구현

--- django_proj_movie/movist/models.py
+++ django_proj_movie/movist/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+# 주연배우
+class Actor(TimestampedModel):
+    name = models.CharField(max_length=10, db_index=True)
+    photo = models.ImageField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "주연배우"
+        verbose_name_plural = "주연배우 목록"
+
+
+class Movie(TimestampedModel):
+    actor = models.ForeignKey(
+        Actor, on_delete=models.CASCADE, verbose_name="주연배우")
+    name = models.CharField(max_length=100)
+    poster = models.ImageField(verbose_name="포스터 이미지")
+    desc = models.TextField(verbose_name="설명")
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Video(TimestampedModel):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    youtube_url = models.URLField()
+
+
+class Review(TimestampedModel):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    message = models.TextField()
