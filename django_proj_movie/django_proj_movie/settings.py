11e719d67b9b38cc1a3ae8a3b2a6713cb89bb154
Chinseok Lee <me@askcompany.kr>
2021-08-10 10:25:59 +0900

    MEDIA 설정

--- django_proj_movie/django_proj_movie/settings.py
+++ django_proj_movie/django_proj_movie/settings.py
@@ -119,6 +119,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'  # django 3 스타일
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
