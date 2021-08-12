4889ec464d522b472d9193a760847a83bd612738
Chinseok Lee <me@askcompany.kr>
2021-08-12 11:53:15 +0900

    django-bootstrap-v5 설정

--- django_proj_movie/django_proj_movie/settings.py
+++ django_proj_movie/django_proj_movie/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap5',
     'debug_toolbar',
     'accounts',
     'movist',
